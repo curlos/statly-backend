150075f81ff52e59ddd63fb0d2857bb1e67dffdc
Carlos Martinez <curlosmart@gmail.com>
2026-01-03 18:12:17 -0500

    Add a "note" property to each "Atomic Habits" focus record.

--- ticktick-demo-account/input/focusRecordsAndCompletedTasks/001-atomic-habits.js
+++ ticktick-demo-account/input/focusRecordsAndCompletedTasks/001-atomic-habits.js
@@ -8,21 +8,24 @@ export default {
                     "Opening Credits",
                     "Introduction: My Story",
                     "Introduction: Opening Credits + My Story"
-                ]
+                ],
+                "note": ""
             },
             {
                 "duration": "24m44s",
                 "taskTitle": "Chapter 1 – The Surprising Power of Atomic Habits",
                 "completedTasks": [
                     "Chapter 1 – The Surprising Power of Atomic Habits"
-                ]
+                ],
+                "note": ""
             },
             {
                 "duration": "19m47s",
                 "taskTitle": "Chapter 2 – How Your Habits Shape Your Identity (and Vice Versa)",
                 "completedTasks": [
                     "Chapter 2 – How Your Habits Shape Your Identity (and Vice Versa)"
-                ]
+                ],
+                "note": ""
             },
             {
                 "duration": "18m50s",
@@ -30,21 +33,24 @@ export default {
                 "completedTasks": [
                     "Chapter 3 – How to Build Better Habits in 4 Simple Steps",
                     "The Fundamentals: Why Tiny Changes Make a Big Difference"
-                ]
+                ],
+                "note": ""
             },
             {
                 "duration": "13m10s",
                 "taskTitle": "Chapter 4 – The Man Who Didn’t Look Right",
                 "completedTasks": [
                     "Chapter 4 – The Man Who Didn’t Look Right"
-                ]
+                ],
+                "note": ""
             },
             {
                 "duration": "15m21s",
                 "taskTitle": "Chapter 5 – The Best Way to Start a New Habit",
                 "completedTasks": [
                     "Chapter 5 – The Best Way to Start a New Habit"
-                ]
+                ],
+                "note": ""
             }
         ],
         "Jan 2, 2024 - 5:00AM": [
@@ -53,7 +59,8 @@ export default {
                 "taskTitle": "Chapter 6 – Motivation Is Overrated; Environment Often Matters More",
                 "completedTasks": [
                     "Chapter 6 – Motivation Is Overrated; Environment Often Matters More"
-                ]
+                ],
+                "note": ""
             },
             {
                 "duration": "09m02s",
@@ -61,21 +68,24 @@ export default {
                 "completedTasks": [
                     "Chapter 7 – The Secret to Self-Control",
                     "The 1st Law: Make It Obvious"
-                ]
+                ],
+                "note": ""
             },
             {
                 "duration": "17m27s",
                 "taskTitle": "Chapter 8 – How to Make a Habit Irresistible",
                 "completedTasks": [
                     "Chapter 8 – How to Make a Habit Irresistible"
-                ]
+                ],
+                "note": ""
             },
             {
                 "duration": "15m35s",
                 "taskTitle": "Chapter 9 – The Role of Family and Friends in Shaping Your Habits",
                 "completedTasks": [
                     "Chapter 9 – The Role of Family and Friends in Shaping Your Habits"
-                ]
+                ],
+                "note": ""
             },
             {
                 "duration": "16m08s",
@@ -83,7 +93,8 @@ export default {
                 "completedTasks": [
                     "Chapter 10 – How to Find and Fix the Causes of Your Bad Habits",
                     "The 2nd Law: Make It Attractive"
-                ]
+                ],
+                "note": ""
             }
         ],
         "Jan 3, 2024 - 5:00AM": [
@@ -92,21 +103,24 @@ export default {
                 "taskTitle": "Chapter 11 – Walk Slowly, but Never Backward",
                 "completedTasks": [
                     "Chapter 11 – Walk Slowly, but Never Backward"
-                ]
+                ],
+                "note": ""
             },
             {
                 "duration": "15m28s",
                 "taskTitle": "Chapter 12 – The Law of Least Effort",
                 "completedTasks": [
                     "Chapter 12 – The Law of Least Effort"
-                ]
+                ],
+                "note": ""
             },
             {
                 "duration": "12m08s",
                 "taskTitle": "Chapter 13 – How to Stop Procrastinating by Using the Two-Minute Rule",
                 "completedTasks": [
                     "Chapter 13 – How to Stop Procrastinating by Using the Two-Minute Rule"
-                ]
+                ],
+                "note": ""
             },
             {
                 "duration": "13m30s",
@@ -114,14 +128,16 @@ export default {
                 "completedTasks": [
                     "Chapter 14 – How to Make Good Habits Inevitable and Bad Habits Impossible",
                     "The 3rd Law: Make It Easy"
-                ]
+                ],
+                "note": ""
             },
             {
                 "duration": "18m26s",
                 "taskTitle": "Chapter 15 – The Cardinal Rule of Behavior Change",
                 "completedTasks": [
                     "Chapter 15 – The Cardinal Rule of Behavior Change"
-                ]
+                ],
+                "note": ""
             }
         ],
         "Jan 4, 2024 - 5:00AM": [
@@ -130,7 +146,8 @@ export default {
                 "taskTitle": "Chapter 16 – How to Stick with Good Habits Every Day",
                 "completedTasks": [
                     "Chapter 16 – How to Stick with Good Habits Every Day"
-                ]
+                ],
+                "note": ""
             },
             {
                 "duration": "11m44s",
@@ -138,21 +155,24 @@ export default {
                 "completedTasks": [
                     "Chapter 17 – How an Accountability Partner Can Change Everything",
                     "The 4th Law: Make It Satisfying"
-                ]
+                ],
+                "note": ""
             },
             {
                 "duration": "19m37s",
                 "taskTitle": "Chapter 18 – The Truth About Talent (When Genes Matter and When They Don’t)",
                 "completedTasks": [
                     "Chapter 18 – The Truth About Talent (When Genes Matter and When They Don’t)"
-                ]
+                ],
+                "note": ""
             },
             {
                 "duration": "12m05s",
                 "taskTitle": "Chapter 19 – The Goldilocks Rule: How to Stay Motivated in Life and Work",
                 "completedTasks": [
                     "Chapter 19 – The Goldilocks Rule: How to Stay Motivated in Life and Work"
-                ]
+                ],
+                "note": ""
             },
             {
                 "duration": "17m04s",
@@ -160,7 +180,8 @@ export default {
                 "completedTasks": [
                     "Chapter 20 – The Downside of Creating Good Habits",
                     "Advanced Tactics: How to Go from Being Merely Good to Being Truly Great"
-                ]
+                ],
+                "note": ""
             },
             {
                 "duration": "06m44s",
@@ -173,7 +194,8 @@ export default {
                     "How to Apply These Ideas to Parenting",
                     "End Credits",
                     "Atomic Habits"
-                ]
+                ],
+                "note": ""
             }
         ]
     }
